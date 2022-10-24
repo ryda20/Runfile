9fbb78186a2cb97c1436cd072c624e9799220c0f
Dien Nguyen <ddnm102@gmail.com>
2022-10-24 11:28:55 +0700

    improve Runfile.template function, adding update current Runfile script instead of replace it

--- run-init.sh
+++ run-init.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
-copy_init_runfile() {
-	cp ~/.Runfile/Runfile . && chmod +x ./Runfile
-}
 
 ## start process ==
 echo "working on: $(pwd)"
+msg=""
 if [ ! -f "./Runfile" ]; then
-	copy_init_runfile
+	msg="could not copy Runfile from ~/.Runfile/Runfile to $(pwd)"
+	cp ~/.Runfile/Runfile . && \
+	chmod +x ./Runfile && \
+	msg="sucessfull"
 else
-	echo -e "Current directory already have Runfile. Remove replace with init Runfile?\n Your data will lost if you replace with init Runfile: [y/n]: "
+	echo -e "Current directory already have Runfile. Update Runfile?[y/n]: "
 	read answer
 	if [ "${answer}" == "y" ]; then
-		copy_init_runfile
-		echo "replace with init Runfile"
+		msg="update Runfile is not sucessful"
+		start_of_file_new=$(sed -n '/MUST_START_OF_FILE$/p' ${RF_DIR:-~/.Runfile}/Runfile)
+		end_of_file_new=$(sed -n '/MUST_END_OF_FILE$/p' ${RF_DIR:-~/.Runfile}/Runfile)
+
+		runfile="./Runfile"
+		cp ${runfile} ${runfile}.tmp && \
+		sed \
+			-e "s,^.*MUST_START_OF_FILE$,${start_of_file_new}," \
+			-e "s,^.*MUST_END_OF_FILE$,${end_of_file_new}," \
+			<${runfile}.tmp >${runfile} && \
+		rm -f ${runfile}.tmp && \
+		echo "Runfile updated" && \
+		msg="Update Runfile was sucessful"
+	else
+		msg="not touch in your Runfile"
 	fi
 fi
 
 if [ ! -f $(pwd)/Runfile ]; then
 	echo "Runfile isn't created! Check above error pls!"
 else
-	echo "Runfile created!"
+	echo "${msg}"
 fi
